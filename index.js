c07dea81d090e370d2d8d760d3a088721ca06ca2
Naveed Hussain <nhussain2@hawk.iit.edu>
2020-05-09 13:05:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // import libraries
 const express = require("express");
 require("./services/passport");
-const authRoutes = require("./routes/authRoutes");
 
-// set up config to listen to incoming reqs for route handlers
+// set up config and pass authRoutes
 const app = express();
-authRoutes(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
