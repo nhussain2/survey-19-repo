bebf3b2db26bb381c922dfbc9963c002b55af3a4
Naveed Hussain <nhussain2@hawk.iit.edu>
2020-05-10 16:58:13 -0700

    finished authflow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js - credentials to return
+
+if (process.env.NODE_ENV === "production") {
+  // we are in prod, return prod set of keys
+  module.exports = require("./prod");
+} else {
+  // we are in dev, return dev set of keys
+  module.exports = require("./dev");
+}
